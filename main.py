8177846dc40234997a4bf2badc57460061e401e2
tivenide <tivenide@gmail.com>
2023-11-14 11:37:09 +0100

    add check_system()

--- main.py
+++ main.py
@@ -7,6 +7,26 @@ def check_cuda():
     else:
         print("CUDA is not available")
 
+def check_system():
+    """
+    Prints available resources.
+    :return: None
+    """
+    import torch
+    import os
+
+    print('Total CPUs (threads) found:',os.cpu_count())
+    print('Available threads for torch:',torch.get_num_threads())
+
+    if torch.cuda.is_available():
+        count = torch.cuda.device_count()
+        print('Total GPUs found:',str(count))
+        for i in range(torch.cuda.device_count()):
+            print(i, torch.cuda.get_device_name(i), torch.cuda.get_device_properties(i))
+    else:
+        print('No GPUs found!')
+
 if __name__ == '__main__':
     check_cuda()
+    check_system()
 
